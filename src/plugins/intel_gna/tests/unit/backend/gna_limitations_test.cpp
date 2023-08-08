ec0de7fec737f516aed6507de9281d6f960f01bc
Szymon Irzabek <szymon.jakub.irzabek@intel.com>
2023-08-08 12:45:55 +0200

    [GNA] Adapt GNA custom DWSC to work with layout transformation

--- src/plugins/intel_gna/tests/unit/backend/gna_limitations_test.cpp
+++ src/plugins/intel_gna/tests/unit/backend/gna_limitations_test.cpp
@@ -381,6 +381,7 @@ struct ValidateCnn2DParams {
                                       parameters["kH"],
                                       parameters["kW"],
                                       parameters["kN"],
+                                      parameters["kG"],
                                       parameters["sH"],
                                       parameters["sW"],
                                       parameters["dH"],
